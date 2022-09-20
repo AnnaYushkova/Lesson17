0abda0dd68229f32bd42cff325f6389d8f5d46ad
AnnaYushkova <annayushkova1991@mail.ru>
2022-09-21 00:30:41 +0300

    Initial Commit

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = "5776175742:AAGeuPqe7ETXjOUfZvsmgUn4jVZ24G8Jm0I"
+keys = {
+    "биткоин" : "BTC",
+    "эфириум" : "ETH",
+    "доллар" : "USD",
+    "евро" : "EUR",
+    "рубль" : "RUB"
+}
---
+++ extensions.py
@@ -0,0 +1,35 @@
+import json
+import requests
+from config import keys
+
+
+
+
+class ConvertionException(Exception):
+    pass
+
+class CryptoConverter:
+    @staticmethod
+    def convert(quote: str, base:str, amount: str):
+        if quote == base:
+            raise ConvertionException(f"Невозможно перевести одинаковые валюты {base}.")
+
+        try:
+            base_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f"Неудалось обработать валюту {quote}.")
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f"Неудалось обработать валюту {base}.")
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f"Неудалось обработать количесво валюты {amount}.")
+
+        r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}")
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base
